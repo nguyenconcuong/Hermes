f6518aa925e014af4fd75fd60cb386de3e1b4d86
nguyenconcuong <nguyen23012000@gmail.com>
2022-08-19 17:25:53 +0700

    search by name

--- iPOSBilling/New Group/DS Khách hàng/DsCusVC.swift
+++ iPOSBilling/New Group/DS Khách hàng/DsCusVC.swift
@@ -9,34 +9,58 @@
 import UIKit
 
 class DsCusVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
-   
+    struct Custumer {
+        var id: String
+        var date: String
+        var name: String
+        var code: String
+        var lead: String
+    }
+    var custmerList: [Custumer] = []
+  
     var id = ["1.", "2.", "3.", "4.", "5.", "6.", "7."]
     var date =  ["01/02/2002", "01/02/2002", "01/02/2002", "01/02/2002", "01/02/2002", "01/02/2002", "01/02/2002"]
     var name = ["1/7", "1012_2@gmail", "1012_4", "1012_5", "1012_6", "1012_7s", "1012_7"]
     var code = ["AZV121212C", "ABC1231212", "UHA213127", "ABC1234", "CBA123", "ACC", "ASMK12341"]
     var lead = ["dao.le@ipos.vn", "dao.le@ipos.vn", "dao.le@ipos.vn", "dao.le@ipos.vn", "dao.le@ipos.vn", "dao.le@ipos.vn", "dao.le@ipos.vn"]
-    var searchname = [String]()
-    var seaching = false
+  
+ 
+    var searchname = [Custumer]()
+    var searchActive = false
+ 
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        searchname = name.filter({$0.prefix(searchText.count) == searchText})
-        
+        searchname = custmerList.filter { (Custumer) -> Bool in
+            return Custumer.name.range(of: searchText, options: [ .caseInsensitive ]) != nil
+        }
+        if(searchText == "" ){
+            searchname = custmerList;
+         } else {
+             searchActive = true;
+         }
+        self.tblDsCus.reloadData()
     }
+
     func numberOfSections(in tableView: UITableView) -> Int {
           return 1
       }
       func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-          return 7
+        return searchname.count
       }
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 150.0
     }
       func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if (!searchActive)
+        {
+            searchname = custmerList
+        }
           let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)as! DsCusTVc
-        cell.txtId.text = id[indexPath.row]
-        cell.txtName.text = name[indexPath.row]
-        cell.txtCode.text = code[indexPath.row]
-        cell.txtLead.text = lead[indexPath.row]
-        cell.txtDate.text = date[indexPath.row]
+        let data = searchname[indexPath.row]
+        cell.txtId.text =  data.id
+        cell.txtName.text = data.name
+        cell.txtCode.text = data.code
+        cell.txtLead.text = data.lead
+        cell.txtDate.text =  data.date
           return cell
       }
       
@@ -60,8 +84,14 @@ class DsCusVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UIS
     @IBOutlet weak var seachView: UIView!
     @IBOutlet weak var tblDsCus: UITableView!
     override func viewDidLoad() {
+        for i in 0..<7 { // this will add 10 elements to contacts
+            custmerList.append(Custumer(id: id[i], date: date[i], name: name[i], code: code[i], lead: lead[i]))
+        }
+        if (!searchActive)
+        {
+            searchname = custmerList
+        }
         super.viewDidLoad()
-       
         let nib = UINib(nibName:"DsCusTVc", bundle: nil)
         tblDsCus.register(nib, forCellReuseIdentifier: "cell")
         tblDsCus.delegate = self
